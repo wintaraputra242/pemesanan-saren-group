fe37ecf8c077e20ea2f5b39f9f9c670d2a597938
wintaraputra242 <wintaraputra242@gmail.com>
2026-09-06 18:48:39 +0800

    fix: update seeder database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,10 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
 
         $this->call(ShieldSeeder::class);
 
